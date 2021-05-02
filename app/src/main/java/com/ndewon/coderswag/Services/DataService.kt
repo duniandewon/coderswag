e4f719eb6ed6fc7049045964ac64d1796a0c1cb8
Abolfaz <duniandewon@gmail.com>
2021-05-02 20:25:45 +0700

    Create product item layout and getProducts function

--- app/src/main/java/com/ndewon/coderswag/Services/DataService.kt
+++ app/src/main/java/com/ndewon/coderswag/Services/DataService.kt
@@ -13,24 +13,33 @@ object DataService {
     )
 
     val hats = listOf(
-        Product("Devsloops Graphic Beanie", "$18", "hat01"),
-        Product("Devsloops Hat Black", "$20", "hat02"),
-        Product("Devsloops Hat White", "$18", "hat03"),
-        Product("Devsloops Snapback", "22", "hat04"),
+        Product("Devsloops Graphic Beanie", "$18", "hat1"),
+        Product("Devsloops Hat Black", "$20", "hat2"),
+        Product("Devsloops Hat White", "$18", "hat3"),
+        Product("Devsloops Snapback", "22", "hat4"),
     )
 
     val hoodies = listOf(
-        Product("Devsloops Hoodie Gray", "$20", "hoodie01"),
-        Product("Devsloops Hoodie Red", "$32", "hoodie02"),
-        Product("Devsloops Gray hoodie", "$20", "hoodie03"),
-        Product("Devsloops Black Hoodie", "$20", "hoodie04"),
+        Product("Devsloops Hoodie Gray", "$20", "hoodie1"),
+        Product("Devsloops Hoodie Red", "$32", "hoodie2"),
+        Product("Devsloops Gray hoodie", "$20", "hoodie3"),
+        Product("Devsloops Black Hoodie", "$20", "hoodie4"),
     )
 
     val shirts = listOf(
-        Product("Devsloops Shirt Black", "$18", "shirt01"),
-        Product("Devsloops Badge Light Gray", "$20", "shirt02"),
-        Product("Devsloops Logo Shirt Red", "$22", "shirt03"),
-        Product("Devsloops Hustle", "$22", "shirt04"),
-        Product("Kickflip Studios", "$18", "shirt05"),
+        Product("Devsloops Shirt Black", "$18", "shirt1"),
+        Product("Devsloops Badge Light Gray", "$20", "shirt2"),
+        Product("Devsloops Logo Shirt Red", "$22", "shirt3"),
+        Product("Devsloops Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "$18", "shirt5"),
     )
+
+    fun getProducts(category: String): List<Product> {
+        return when(category) {
+            "shirts" -> shirts
+            "hats" -> hats
+            "hoodies" -> hoodies
+            else -> listOf<Product>()
+        }
+    }
 }
