d425541895c121937540bc0cfb5b670f9afa3b30
Abolfaz <duniandewon@gmail.com>
2021-05-02 19:56:22 +0700

    ÃCreate products activity and transition to it

--- app/src/main/java/com/ndewon/coderswag/Controllers/MainActivity.kt
+++ app/src/main/java/com/ndewon/coderswag/Controllers/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.ndewon.coderswag.Controllers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ndewon.coderswag.Adapters.CategoryAdapter
 import com.ndewon.coderswag.R
 import com.ndewon.coderswag.Services.DataService
+import com.ndewon.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         categoryListView.adapter = CategoryAdapter(this, DataService.categories) { category ->
-            println(category.title)
+            val productIntent = Intent(this, ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
         }
         categoryListView.layoutManager = LinearLayoutManager(this)
         categoryListView.setHasFixedSize(true)
--- app/src/main/java/com/ndewon/coderswag/Controllers/ProductsActivity.kt
+++ app/src/main/java/com/ndewon/coderswag/Controllers/ProductsActivity.kt
@@ -3,10 +3,15 @@ package com.ndewon.coderswag.Controllers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ndewon.coderswag.R
+import com.ndewon.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+
+        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+
+        println(categoryType)
     }
 }
